f6da3a1c876655c324055f295508e38b95c3475b
chenthg1204 <chenthg@gmail.com>
2026-09-06 15:27:27 +0800

    Update market_director.py
    bot key name change

--- market_director.py
+++ market_director.py
@@ -9,7 +9,7 @@ import requests
 from groq import Groq
 
 # 從環境變數安全讀取金鑰
-TELEGRAM_BOT_OPENBIG_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_OPENBIG_TOKEN")
 TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
 GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
 
